1dccd4c9e54c04f45b0e5f9f81631cecebbc0c50
plump_albert <plumpalbert@gmail.com>
2019-05-26 12:24:17 +0300

    Второй пункт ЛР2

--- lab_2.py
+++ lab_2.py
@@ -5,7 +5,7 @@ import sympy as sp
 def main(f, systemX, X, optX):
     x1, x2 = X[0:2]
     document = open('lab_2.md', 'w')
-    
+
     document.write(
         '---\n' +
         'mainfont: Google Sans\n' +
@@ -24,7 +24,7 @@ def main(f, systemX, X, optX):
         'Область ограничений в стандартной форме:\n' +
         r'$$X = \begin{cases}' +
         r',\\'.join([sp.latex(s) for s in systemX]) + r',\\' +
-        ','.join([sp.latex(x) for x in X]) +
+        ','.join([sp.latex(x) for x in X[0:2]]) +
         r'\geq 0.\end{cases}\\$$'
     )
 
@@ -57,5 +57,77 @@ def main(f, systemX, X, optX):
         '\n\n# Используя теоремы двойственности, \
       найдем решение задачи линейного программирования\n' +
         'Найденное оптимальное решение в лабораторной работе № 1 :\n\n' +
-        '$x^* = (%d, %d)$' % (optX[x1], optX[x2])
+        '$x^*=(%d, %d), f(x^*)=%d$' % (optX[x1], optX[x2],
+                                       f.subs({x1: optX[x1], x2: optX[x2]}))
     )
+    document.write(
+        '\n\nТеорема о дополнительной нежесткости:\n\n'
+    )
+    theorem = []
+    for i, s in enumerate(systemY):
+        theorem.append(sp.Equality(
+            sp.Mul(optX[X[i]], (s.lhs - s.rhs), evaluate=False), 0))
+    for i, s in enumerate(systemX):
+        left = Y[i] * sp.Add(s.lhs.subs({p: optX[p]
+                                         for p in X[0:2]}), -s.rhs, evaluate=False)
+        theorem.append(sp.Equality(left, 0, evaluate=False))
+
+    theoremY = theorem[2:]
+    _y = []
+    __y = []
+    for i, s in enumerate(theoremY):
+        if s.doit() == True:
+            _y.append(Y[i])
+        else:
+            __y.append(Y[i])
+
+    document.write(
+        r'$\begin{cases}' +
+        r',\\'.join([sp.latex(s) for s in theorem]) +
+        r'.\end{cases}  \Rightarrow' +
+        r'\begin{cases}' +
+        r',\\'.join([sp.latex(_) + r' ^ * \neq 0 ' for _ in _y]) + r',\\' +
+        r',\\'.join([sp.latex(_) + '^* = 0' for _ in __y]) +
+        r'.\end{cases}$'
+    )
+
+    document.write(
+        '\n\nНайдем значения $' +
+        '$ и $'.join([sp.latex(e)+'^*' for e in _y]) + '$:\n\n'
+    )
+
+    resultY = {p: 0 for p in __y}
+    solveY = [sp.Equality(
+        s.lhs.subs(resultY),
+        s.rhs
+    ) for s in systemY]
+    resultY.update(dict(zip([e for e in _y], *sp.linsolve(solveY, _y))))
+    document.write(
+        r'$$\begin{cases}' +
+        r',\\'.join([sp.latex(sp.Equality(s.lhs, s.rhs)) for s in systemY]) +
+        r'.\end{cases}' +
+        r'\Rightarrow \begin{cases}' +
+        r',\\'.join([sp.latex(s) for s in solveY]) +
+        r'.\end{cases}' +
+        r'\Rightarrow \begin{cases}' +
+        r',\\'.join([sp.latex(sp.Equality(p, resultY[p])) for p in _y]) +
+        r'.\end{cases} \Rightarrow ' +
+        r'y^*(' +
+        r','.join([sp.latex(resultY[sp.symbols('y%d' % (i + 1))]) for i in range(len(resultY))]) +
+        r')\\$$' + '\n\n'
+    )
+    document.write(
+        r'$$\varphi(y^*)=' +
+        sp.latex(phi) + '=' +
+        sp.latex(phi.subs({p: sp.symbols(str(resultY[p])) for p in resultY})) + '=' +
+        sp.latex(phi.subs(resultY)) +
+        r'\\$$'
+    )
+    if phi.subs(resultY) == f.subs(optX):
+        document.write(
+            '\n\nЗначения совпали:' +
+            r'$f(x^*)=\varphi(y^*)={0}$'.format(phi.subs(resultY))
+        )
+    else:
+        document.write('<h3 style="color: crimson">Соси член, кусок долбаеба.\
+        Какую-то хуйню сделал... Ищи ошибку!</h3>')
