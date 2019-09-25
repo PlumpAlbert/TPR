bfcd883d8d66307d4ba6d731b32144387293f190
plump_albert <plumpalbert@gmail.com>
2019-09-25 07:58:37 +0300

    Killing converter and preview processes at the end

--- live.sh
+++ live.sh
@@ -2,8 +2,11 @@
 
 xelatex $1
 (ls "$1" | entr -pc xelatex $1) &
+CONVERTER="$!"
 BASE_FILE_NAME=${1%.*}
 mupdf "$BASE_FILE_NAME.pdf" &
 MUPDF="$!"
 ls "$BASE_FILE_NAME.pdf" | entr -c -p kill -SIGHUP "$MUPDF"
 
+kill -SIGTERM "$CONVERTER"
+kill -SIGTERM "$MUPDF"
